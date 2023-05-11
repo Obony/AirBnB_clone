829c76d63b1450f85cb18e1649b942f2f536aeb4
Obony <faithnyabz69@gmail.com>
2023-05-10 21:55:31 -0700

    corrections

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,22 +17,22 @@ class FileStorage:
     def all(self):
         """return __objects"""
 
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """adds a new object to __objects dictionary"""
 
         key = str(obj.__class__.__name__) + '.' + str(obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
 
         from json import dump
 
-        file_name = self.__file_path
+        file_name = FileStorage.__file_path
 
-        to_json = dict(self.__objects)
+        to_json = dict(FileStorage.__objects)
         for k, v in to_json.items():
             to_json[k] = v.to_dict()
 
@@ -51,7 +51,7 @@ class FileStorage:
         from models.review import Review
         from models.state import State
 
-        file_name = self.__file_path
+        file_name = FileStorage.__file_path
         try:
             with open(file_name, "r", encoding="UTF-8") as f:
                 from_json = load(f)
@@ -71,6 +71,6 @@ class FileStorage:
                     elif 'Review' in k:
                         from_json[k] = Review(**v)
 
-                self.__objects = dict(from_json)
+                FileStorage.__objects = dict(from_json)
         except FileNotFoundError:
             pass
